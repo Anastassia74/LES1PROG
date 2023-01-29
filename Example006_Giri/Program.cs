accfa106a596d8a85c44be2bb7cdf757815edb44
AnastassiaM <pupanda74@gmail.com>
2023-01-29 12:34:41 +0300

    Example006_giri

---
+++ Example006_Giri/Program.cs
@@ -0,0 +1,15 @@
+﻿int a=1;
+int b=39;
+int c=8;
+int d=124;
+int e=356;
+int max=a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+Console.Write("Максимальное значение: ");
+Console.WriteLine(max);
+    
