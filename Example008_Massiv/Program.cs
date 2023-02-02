ad9e32dc072c6f70396f51b837e1c178aee22a73
AnastassiaM <pupanda74@gmail.com>
2023-02-03 00:34:51 +0300

    Добавили функции и массивы

---
+++ Example008_Massiv/Program.cs
@@ -0,0 +1,39 @@
+﻿int Maxim (int arg1, int arg2, int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
+int a1=15;
+int b1=10;
+int c1=12;
+int a2=15500;
+int b2=10;
+int c2=40;
+int a3=77731;
+int b3=210;
+int c3=46;
+
+// int max1 = Maxim (a1, b1, c1);
+// int max2 = Maxim (a2, b2, c2);
+// int max3 = Maxim (a3, b3, c3);
+// int maxtotal = Maxim (max1, max2, max3);
+
+int maxtotal = Maxim (
+    Maxim (a1, b1, c1),
+    Maxim (a2, b2, c2), 
+    Maxim (a3, b3, c3));
+System.Console.WriteLine(maxtotal);
+
+// int max =a1;
+// if (b1>max) max=b1;
+// if (c1>max) max=c1;
+// if (a2>max) max=a2;
+// if (b2>max) max=b2;
+// if (c2>max) max=c2;
+// if (a3>max) max=a3;
+// if (b3>max) max=b3;
+// if (c3>max) max=c3;
+//System.Console.WriteLine(max);
---
+++ Example009_Mas/Program.cs
@@ -0,0 +1,20 @@
+﻿int Maxim (int arg1, int arg2, int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
+//              0  1  2  3 4  5   6    7  8                
+int[] array = {10,11,19,9676,36,89,105,1987,6};
+array[4]=12;
+System.Console.WriteLine(array[4]);
+
+int maxtotal = Maxim (
+    Maxim (array[0], array[1], array[2]),
+    Maxim (array[3], array[4], array[5]), 
+    Maxim (array[6], array[7], array[8])
+    );
+System.Console.WriteLine(maxtotal);
+
