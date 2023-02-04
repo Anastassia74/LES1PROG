fd02ab92f59782e43dcc8b438f258026efdcbfc1
AnastassiaM <pupanda74@gmail.com>
2023-02-04 22:11:58 +0300

    Добавили функции и массивы 10 и 11

---
+++ Example010_Find/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {11,2,23,81,65,36,17,81};
+int n = array.Length;
+int find = 81;
+int index = 0;
+while (index<n)
+{
+    if (array [index]==find)
+    {
+    System.Console.WriteLine(index);
+    break;
+    }
+    //index=index+1;
+    index++;
+}
